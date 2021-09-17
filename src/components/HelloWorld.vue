15ab14e971e39c7bd2137a23e0a1bfa112e68f0d
小曹 <7835271+xiao_cao333@user.noreply.gitee.com>
2021-09-17 19:38:42 +0800

    上传文件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,63 @@
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="state.count++">
-    count is: {{ state.count }}
-  </button>
-  <p>
-    hello xiaocao
-  </p>
+  <div class="upload">
+      <label for="">
+        <p>输入传送地址（完整的）先输入地址再放图片发的get请求</p>
+        <a-input v-model:value="url" @change="onchange" style="width:300px;height:40px"/>
+      </label>
+      <a-upload-dragger
+          v-model:fileList="fileList"
+          name="file"
+          :multiple="true"
+          method='get'
+          :action="url"
+          @change="handleChange"
+        >
+          <p class="ant-upload-drag-icon">
+            <inbox-outlined></inbox-outlined>
+          </p>
+          <p class="ant-upload-text">点击或者拖拽上传！</p>
+          <p class="ant-upload-hint">
+            点击上传图片或者拖拽上传
+          </p>
+      </a-upload-dragger>
+  </div>
 </template>
 
-<script setup>
-import { defineProps, reactive } from 'vue'
+<script>
 
-defineProps({
-  msg: String
+import { defineComponent, defineProps, reactive } from 'vue'
+import { message } from 'ant-design-vue'
+
+export default defineComponent({
+  name:'HelloWorld',
+  data(){
+    return {
+      url:''
+    }
+  },
+  methods: {
+    handleChange(){
+      if (this.url == ''){
+        message.info('你先输入地址！')
+        return false
+      }else{
+        
+      }
+    }
+  }
 })
-  
-const state = reactive({ count: 0 })
 </script>
 
 <style scoped>
 a {
   color: #42b983;
+}
+.upload{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  margin: 0 auto;
 }
 </style>
